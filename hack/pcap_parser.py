3b9093bc5190d2807cb6a2d8278f573dc0486114
Nyloner <longni@hustunique.com>
2016-06-08 11:20:05 +0800

    update

--- hack/pcap_parser.py
+++ hack/pcap_parser.py
@@ -12,21 +12,29 @@ def getHttpHeaders(http_payload):
         return None
     return headers
 
-def extractImage(headers,http_payload):
+def extractImage(headers,data):
     image=None
     image_type=None
     try:
         if 'image' in headers['Content-Type']:
             image_type=headers['Content-Type'].split('/')[1]
-            image=http_payload[http_payload.index('\r\n\r\n')+4:]
-            try:
-                if 'Content-Encoding' in headers.keys():
-                    if headers['Content-Encoding']=='gzip':
-                        image=zlib.decompress(image,16+zlib.MAX_WBITS)
-                    elif headers['Content-Encoding']=='deflate':
-                        image=zlib.decompress(image)
-            except:
-                pass
+            startindex=None
+            endindex=None
+            if image_type=='png' or image_type=='PNG':
+                for index in range(len(data)):
+                    if data[index]==0x89 and data[index+1]==0x50 and data[index+2]==0x4e and data[index+3]==0x47:
+                        startindex=index
+                image=data[startindex:]
+            if image_type=='jpeg':
+                for index in range(len(data)):
+                    if data[index]==0xff and data[index+1]==0xd8:
+                        startindex=index
+                    if data[index]==0xff and data[index+1]==0xd9:
+                        endindex=index+1
+                try:
+                    image=data[startindex:endindex]
+                except:
+                    image=data[startindex:]
     except:
         return None,None
     return image,image_type
@@ -37,22 +45,21 @@ def httpAssembler(pacp_file):
     a=rdpcap(pacp_file)
     sessions=a.sessions()
     for session in sessions:
-        http_payload=''
+        http_payload=b''
         for packet in sessions[session]:
             try:
                 if(packet[TCP].dport==80 or packet[TCP].sport==80):
-                    http_payload+=str(packet[TCP].payload)
+                    http_payload+=bytes(packet[TCP].payload)
             except:
                 pass
-        http_payload=re.sub('\\\\r\\\\n','\\r\\n',http_payload)
-        headers=getHttpHeaders(http_payload)
+        headers=getHttpHeaders(http_payload.decode('utf-8','ignore'))
         if headers is None:
             continue
         print(headers)
         image,image_type=extractImage(headers,http_payload)
         if image is not None and image_type is not None:
             filename='%s-pic_%d.%s'%(pacp_file,carved_images,image_type)
-            img=open('%s/%s'%(imgdir,filename),'w')
+            img=open('%s/%s'%(imgdir,filename),'wb')
             img.write(image)
             img.close()
             carved_images+=1
--- hack/sniffer.py
+++ hack/sniffer.py
@@ -1,6 +1,6 @@
 from scapy.all import *
-
-
+import time
+from scapy.utils import PcapReader, PcapWriter
 #sniff(filter="",iface='any',prn=function,count=N)
 
 def getHttpHeaders(http_payload):
@@ -19,15 +19,55 @@ def getUser(packet):
     f.write("[*]Data:{}".format(packet[IP].payload)+'\n\n\n')
     f.close()
 
-def packet_callback(packet):
-    if packet[TCP].payload:
-        data=packet[TCP].load.decode('utf-8','ignore')
-        headers=getHttpHeaders(data)
-        if headers==None:
-            return
-        passwd_packet=str(packet[TCP].payload)
-        if  'password' in passwd_packet.lower() or 'passwd' in passwd_packet.lower() or 'username' in passwd_packet.lower():
-            getUser(packet)
-        print(packet[IP].load)
+def getImage(packet,headers):
+    data=packet.payload
+    data=bytes(data)
+    image_type=headers['Content-Type'].split('/')[1]
+    startindex=None
+    endindex=None
+    image=None
+    if image_type=='png' or image_type=='PNG':
+        for index in range(len(data)):
+            if data[index]==0x89 and data[index+1]==0x50 and data[index+2]==0x4e and data[index+3]==0x47:
+                startindex=index
+        image=data[startindex:]
+    if image_type=='jpeg':
+        for index in range(len(data)):
+            if data[index]==0xff and data[index+1]==0xd8:
+                startindex=index
+            if data[index]==0xff and data[index+1]==0xd9:
+                endindex=index+1
+        try:
+            image=data[startindex:endindex]
+        except:
+            image=data[startindex:]
+    if image==None:
+        return
+    imgname=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
+    img=open('images/%s.%s'%(imgname,image_type),'wb')
+    img.write(image)
+    img.close()
 
-packets=sniff(filter='tcp port 80',prn=packet_callback,count=100000)
+def packet_callback(packet):
+    try:
+        statue=packet[TCP].load
+    except:
+        return
+    data=packet[TCP].load.decode('utf-8','ignore')
+    headers=getHttpHeaders(data)
+    if headers==None:
+        return
+    passwd_packet=str(packet[TCP].payload)
+    if  'password' in passwd_packet.lower() or 'passwd' in passwd_packet.lower() or 'username' in passwd_packet.lower():
+        getUser(packet)
+    try:
+        if 'image' in headers['Content-Type']:
+            getImage(packet,headers)
+    except:
+        pass
+    writer=PcapWriter('data.pcap', append = True)
+    writer.write(packet)
+    writer.flush()
+    writer.close()
+
+sniff(filter='',prn=packet_callback,count=100000)
